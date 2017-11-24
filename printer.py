bcca74c51da32fa038276a87271d0e8110155b64
Dominik Schmidtlein <dominik.schmidtlein@gmail.com>
2017-11-24 00:04:06 -0500

    finished testing calculator and fixed parts of printer

--- printer.py
+++ printer.py
@@ -5,27 +5,42 @@ class Printer:
         pass
 
     def print_transactions(self, transactions):
-        table = PrettyTable(['Symbol', 'Purchase Quantity', 'Purchase Value', 'Current Price', 'Average Entry Price'])
-        table.align.update({'Purchase Quantity': 'r', 'Purchase Value': 'r', 'Current Price': 'r', 'Average Entry Price': 'r'})
+        columns = ['Symbol', 'Purchase Quantity', 'Purchase Value', 'Current Price', 'Average Entry Price']
+        f = self._formatter(2)
+        table = PrettyTable(columns)
+        table.align.update({ c: 'r' for c in columns[1:] })
         for t in transactions:
-            table.add_row([t['symbol'], t['purchaseQuantity'], t['purchaseValue'], t['currentPrice'], t['averageEntryPrice']])
+            table.add_row([t['symbol'],
+                           t['purchaseQuantity'],
+                           f(t['purchaseValue']),
+                           f(t['currentPrice']),
+                           f(t['averageEntryPrice'])])
         print table
 
     def print_breakdown(self, positions):
-        table = PrettyTable(['Symbol', 'Market Value (CAD)', 'Actual %', 'Ideal %'])
-        table.align.update({'Market Value (CAD)': 'r', 'Actual %': 'r', 'Ideal %': 'r'})
+        columns = ['Symbol', 'Market Value (CAD)', 'Actual %', 'Ideal %']
+        f = self._formatter(2)
+        table = PrettyTable(columns)
+        table.align.update({ c: 'r' for c in columns[1:] })
         for p in positions:
             table.add_row([
                 p['symbol'],
-                round(p['marketValue'], 2),
-                round(p['actual %'], 1),
-                round(p['ideal %'], 1)])
+                f(p['marketValue']),
+                f(p['actual %']),
+                f(p['ideal %'])])
         print table
 
-    def print_balances(self, old_balances, new_balances):
-        table = PrettyTable(['Balance', 'Before', 'After'])
-        table.align.update({'Balance': 'l', 'Before': 'r', 'After': 'r'})
-        table.add_row(['Cash', round(old_balances['cash'], 2), round(new_balances['cash'], 2)])
-        table.add_row(['Market Value', round(old_balances['marketValue'], 2), round(new_balances['marketValue'], 2)])
-        table.add_row(['Total Equity', round(old_balances['totalEquity'], 2), round(new_balances['totalEquity'], 2)])
+    def print_balances(self, balances):
+        columns = ['Balance', 'Before', 'After']
+        f = self._formatter(2)
+        table = PrettyTable(columns)
+        table.align.update({ c: 'r' for c in columns[1:] })
+        table.add_row(['Cash',         f(balances['cash']),        f(balances['newCash'])])
+        table.add_row(['Market Value', f(balances['marketValue']), f(balances['newMarketValue'])])
+        table.add_row(['Total Equity', f(balances['totalEquity']), f(balances['totalEquity'])])
         print table
+
+    def _formatter(self, decimals):
+        def f(value):
+            return "{0:.{1}f}".format(value, decimals)
+        return f
